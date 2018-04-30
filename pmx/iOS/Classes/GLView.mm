a6ca98e86ae16c00d2209c29bb4c4873e25e7cc2
Guy Barrand <guy.barrand@gmail.com>
2018-04-30 20:12:23 +0200

    update

--- pmx/iOS/Classes/GLView.mm
+++
@@ -1,8 +0,0 @@
-// Copyright (C) 2010, Guy Barrand. All rights reserved.
-// See the file pmx.license for terms.
-
-#include "../../pmx/main"
-
-#define EXLIB_APP pmx
-
-#include <exlib/app/iOS/GLView_mm>
