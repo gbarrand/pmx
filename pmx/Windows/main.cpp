4ae3d59243dd66450af7de7ca41d1ea81c8088bc
Guy Barrand <guy.barrand@gmail.com>
2021-08-26 18:31:51 +0200

    update

--- pmx/Windows/main.cpp
+++ pmx/Windows/main.cpp
@@ -5,9 +5,10 @@
 
 #include <exlib/app/Windows/main_cpp>
 
-int main(int argc,char** argv) {return exlib_main<pmx::context,pmx::main>("pmx",argc,argv);}
+int main(int argc,char** argv) {return exlib_main<pmx::main>("pmx",argc,argv);}
 
 //exlib_build_use inlib expat
 //exlib_build_use exlib png jpeg zlib inlib_glutess freetype
-//exlib_build_use GL Windows kernel
+//exlib_build_use WGL Windows kernel
+//exlib_build_bigobj
 
